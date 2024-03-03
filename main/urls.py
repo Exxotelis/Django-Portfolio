14787ad7fee063406ef9dbf9d7888659c7b9a867
telis <telis0kaps@gmail.com>
2024-03-03 14:09:00 +0000

    root

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,17 @@
 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import path, include
 from . import views
-
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about-me.html', views.about_me, name='about_me'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += [path('admin/', admin.site.urls)]
